4fa4c4e7c1a68b9e6a7c129bda63a2bc0a2e22eb
leghadjeu.christian <legochristianarthur@gmail.com>
2025-02-28 08:27:41 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // Access the parameters passed to the GitHub Action
+    let param1 = env::var("INPUT_PARAM1").unwrap_or_else(|_| String::from("default_value1"));
+    let param2 = env::var("INPUT_PARAM2").unwrap_or_else(|_| String::from("default_value2"));
+
+    println!("Parameter 1: {}", param1);
+    println!("Parameter 2: {}", param2);
+
+    // Your logic here
 }
